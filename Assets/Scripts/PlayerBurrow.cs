bc3bf8a803316253781f95e601eb1f71368b6f07
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2024-01-09 22:47:55 +0000

    mas sonidos y musica

--- Assets/Scripts/PlayerBurrow.cs
+++ Assets/Scripts/PlayerBurrow.cs
@@ -6,12 +6,26 @@ public class PlayerBurrow : MonoBehaviour
 {
     public bool onBurrow = false;
 
+    public AudioSource burrowSound;  
+    public AudioClip burrowClip;
+
+
+    private void Start()
+    {
+        
+        burrowSound = gameObject.AddComponent<AudioSource>();
+        burrowSound.playOnAwake = false;
+        burrowSound.clip = burrowClip;
+    }
+
     private void OnTriggerEnter2D(Collider2D entry)
     {
         if (entry.CompareTag("Burrow"))
         {
             onBurrow = true;
             //Debug.Log("Ha entrado en la madriguera.");
+            burrowSound.Play();
+
         }
     }
 

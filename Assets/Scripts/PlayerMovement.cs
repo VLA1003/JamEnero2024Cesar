e5a8200d5d76b5dc5a054d84ef736413a6980a37
VLA1003 <114227795+VLA1003@users.noreply.github.com>
2024-01-09 19:02:20 +0000

    Personaje animado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,10 +14,14 @@ public class PlayerMovement : MonoBehaviour
     public Image staminaBar, poopCover;
     float stamina = 3f, maxStamina = 3f;
     bool running = false, canRun = true;
+    public bool isMoving = false;
+
+    public Animator animator;
 
     private void Start()
     {
         poop.SetActive(false);
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -25,19 +29,18 @@ public class PlayerMovement : MonoBehaviour
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-        if (horizontalMovement != 0f && verticalMovement != 0f)
+        if (horizontalMovement != 0f || verticalMovement != 0f)
         {
-            if (Mathf.Abs(horizontalMovement) > Mathf.Abs(verticalMovement))
-            {
-                verticalMovement = 0f;
-            }
-            else
-            {
-                horizontalMovement = 0f;
-            }
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
         }
         Vector3 movement = new Vector3(horizontalMovement, verticalMovement, 0f) * speed * Time.deltaTime;
         gameObject.GetComponent<Rigidbody2D>().MovePosition(transform.position + movement * speed * Time.deltaTime);
+
+        UpdateAnimatorParameters(horizontalMovement, verticalMovement);
     }
 
     private void Update()
@@ -121,6 +124,13 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+    }
+
+    private void UpdateAnimatorParameters(float horizontal, float vertical)
+    {
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetBool("IsMoving", isMoving);
     }
 
     void DropPoop()

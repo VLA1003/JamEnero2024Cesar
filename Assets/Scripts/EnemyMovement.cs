4750c57ae0bca3a509f6c2f3bd474f93834d8308
VLA1003 <11alvaro11alonso@gmail.com>
2024-01-10 12:57:38 +0000

    Movimiento enemigos
    Movimiento de los enemigos. Hay que crear puntos de patrulla para dictaminar por dónde se moverá el enemigo mientras patrulla.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -14,10 +14,13 @@ public class EnemyMovement : MonoBehaviour
     private int currentPatrolIndex = 0;
     private float timer;
 
+    private Animator animator;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timer = chaseDuration;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -38,6 +41,7 @@ public class EnemyMovement : MonoBehaviour
 
         if (distanceToPlayer < visionRadius)
         {
+            SetAnimationParameters(player.position - transform.position);
             return true;
         }
 
@@ -47,7 +51,10 @@ public class EnemyMovement : MonoBehaviour
     void ChasePlayer()
     {
         timer -= Time.deltaTime;
+        Vector2 direction = player.position - transform.position;
         transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        SetAnimationParameters(direction);
+
         if (timer <= 0f)
         {
             timer = chaseDuration;
@@ -57,11 +64,19 @@ public class EnemyMovement : MonoBehaviour
 
     void Patrol()
     {
+        Vector2 direction = patrolPoints[currentPatrolIndex].position - transform.position;
         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, patrolSpeed * Time.deltaTime);
+        SetAnimationParameters(direction);
+
         if (Vector2.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 0.2f)
         {
             currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
         }
     }
-}
 
+    void SetAnimationParameters(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        animator.SetFloat("Direction", angle);
+    }
+}
